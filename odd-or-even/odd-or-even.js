aaac1c0275b33fdc2fad167c0bb2207eaf692335
Roman <romansenin15@gmail.com>
2020-10-07 16:05:03 -0700

    wrote solution

---
+++ odd-or-even/odd-or-even.js
@@ -0,0 +1,6 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+const oddOrEven = function (num) {
+  return num % 2 === 0 ? "even" : "odd";
+};
