7ba847defef8ae6986dd00a303203852f00ba611
Roman <romansenin15@gmail.com>
2020-10-19 22:18:03 -0700

    wrote fizz-buzz solution

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,19 @@
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+
+var fizzBuzz = function (arr) {
+  arr.forEach((number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else if (number % 3 === 0) {
+      console.log("Fizz");
+    } else if (number % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(number);
+    }
+  });
+};
